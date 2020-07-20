473fda95ad965417e3787d34f27430472545d003
Tiago Lima <tiaguinho.alima@gmail.com>
2020-07-20 20:22:00 -0300

    Solução dos problemas da URI
    Signed-off-by: Tiago Lima <tiaguinho.alima@gmail.com>

--- Estruturas e Bibliotecas/URI_1025.py
+++ Estruturas e Bibliotecas/URI_1025.py
@@ -1,39 +1,24 @@
-def busca(peças, chute, n):
-    b = n // 2
-    p = 0
-    while True:
-        if chute >= peças[b]:
-            peças = peças[b:]
-            p += b
-        elif chute < peças[b]:
-            peças = peças[:b]
-        b //= 2
+def busca(peças, chute):
+    from bisect import bisect_left as bl
+    pos = bl(peças, chute)
 
-        if len(peças) <= 10 or chute == peças[b]:
-            if chute in peças:
-                p += peças.index(chute) + 1
-                return p
-            else:
-                return -1
+    if pos < len(peças) and peças[pos] == chute:
+        return pos + 1
+    else:
+        return -1
 
 
 case = 1
 while True:
     quant, tent = map(int, input().split())
-    if quant == 0 and tent == 0 or case > 65 or quant > 10000 or quant < 0 or tent < 0:
+    if quant == 0 and tent == 0:
         break
 
-    marbles = []
-    for m in range(quant):
-        marbles.append(int(input()))
+    marbles = [int(input()) for m in range(quant)]
     marbles.sort()
-    chutes = []
-    for c in range(tent):
-        chutes.append(int(input()))
-
     print('CASE# {}:'.format(case))
-    for ch in chutes:
-        r = busca(marbles, ch, quant)
-        print('{} {}'.format(ch, 'found at {}'.format(r) if r != -1 else 'not found'))
+    for ch in range(tent):
+        t = int(input())
+        r = busca(marbles, t)
+        print('{} {}'.format(t, 'found at {}'.format(r) if r != -1 else 'not found'))
     case += 1
-
