b83ddd3bc44dd6b32cbf75fbc3c261e6839c4c5e
Tiago Lima <tiaguinho.alima@gmail.com>
2020-07-30 23:04:11 -0300

    Solução dos problemas da URI (Outputs corretos, mas dando 100% wrong. Em análise no fórum)
    Signed-off-by: Tiago Lima <tiaguinho.alima@gmail.com>

--- Estruturas e Bibliotecas/URI_1023.py
+++ Estruturas e Bibliotecas/URI_1023.py
@@ -1,43 +1,50 @@
-def ordem(p, f, n):
-    t = len(f)
-    juntos = [(p[item], f[item]) for item in range(t)]
-
-    juntos.sort(key=lambda x: x[1])
-
-    print('Cidade# {}:'.format(n))
-    for i in juntos:
-        print('{}-{}'.format(i[0], i[1]), end=' ')
-    print()
+def correcao(x):
+    inteiro, decimal = x.split('.')
+    decimal = decimal[:2]
+    valor = inteiro + '.' + decimal
+    return float(valor)
 
 
-n = 1
+def estiagem(ca, first=True):
+    mensal = moradores = 0
+
+    for o in range(ca):
+        m, cm = map(int, input().split())
+        if 1 <= m <= 10 and 1 <= cm <= 200:
+
+            faixa = cm // m
+            mensal += cm
+            moradores += m
+
+            if first:
+                faixas[faixa] = m
+                first = False
+            elif faixa not in faixas:
+                faixas[faixa] = m
+            else:
+                faixas[faixa] += m
+
+    md = correcao(str(mensal / moradores))
+    return md
+
+
+cidade = 1
 while True:
     casos = int(input())
-    if casos == 0 or 0 > casos or casos > 1000000:
+    if casos == 0:
         break
 
-    con_total = total_m = 0
-    faixa, pessoa = list(), list()
+    global faixas
+    faixas = dict()
+    media = estiagem(casos)
 
-    for v in range(casos):
-        q_moradores, con_mensal = map(int, input().split())
-
-        if 1 <= q_moradores <= 10 and 1 <= con_mensal <= 200:
-
-            con_total += con_mensal
-            total_m += q_moradores
-            m3 = con_mensal // q_moradores
-
-            if m3 not in faixa:
-                faixa.append(m3)
-                pessoa.append(q_moradores)
-            else:
-                pos = faixa.index(m3)
-                pessoa.insert(pos, pessoa[pos] + q_moradores)
-                pessoa.pop(pos+1)
-
-    ordem(pessoa, faixa, n)
-    med = con_total / total_m
-
-    print('Consumo medio: {:.2f} m3.'.format(med))
-    n += 1
+    faixas = {key: faixas[key] for key in sorted(faixas)}
+    print('Cidade# {}:'.format(cidade))
+    trava = 1
+    for c, p in faixas.items():
+        print('{}-{}'.format(p, c), end=(' ' if trava < len(faixas) else ''))
+        trava += 1
+    print()
+    print('Consumo medio: {:.2f} m3.'.format(media))
+    print()
+    cidade += 1
