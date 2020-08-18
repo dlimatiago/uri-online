26c16155bac4b3eb9164de169813d5b757b4cae2
Tiago Lima <tiaguinho.alima@gmail.com>
2020-08-18 18:51:44 -0300

    Solução dos problemas da URI
    Signed-off-by: Tiago Lima <tiaguinho.alima@gmail.com>

---
+++ Iniciante/URI_1557.py
@@ -0,0 +1,25 @@
+from Teste.Testador import Testes
+
+t = Testes('C:/Users/Tiago/PycharmProjects/URIonline/Teste/Respostas.txt')
+
+while True:
+    ordem = int(t.proximoCasoinput())
+    if ordem == 0:
+        break
+    matriz = []
+    i, f = 0, ordem
+    for _ in range(ordem):
+        matriz.append(list(map(lambda x: 2 ** x, list(range(i, f)))))
+        i += 1
+        f += 1
+
+    T = len(str(matriz[ordem - 1][ordem - 1]))
+    for i in range(ordem):
+        for j in range(ordem):
+            matriz[i][j] = str(matriz[i][j])
+            while len(matriz[i][j]) < T:
+                matriz[i][j] = ' ' + matriz[i][j]
+        M = ' '.join(matriz[i])
+
+        print(M)
+    print()
