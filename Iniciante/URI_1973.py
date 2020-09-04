7251b23abaf306dd8a55a69595a2a89e44d8e5b3
Tiago Lima <tiaguinho.alima@gmail.com>
2020-09-04 13:04:53 -0300

    Solução dos problemas da URI
    Signed-off-by: Tiago Lima <tiaguinho.alima@gmail.com>

---
+++ Iniciante/URI_1973.py
@@ -0,0 +1,22 @@
+# from Teste.Testador import Testes
+# t = Testes('C:/Users/Tiago/PycharmProjects/URIonline/Teste/Respostas.txt')
+
+stars = int(input())
+lambs = [int(i) for i in input().split()]
+tlambs = sum(lambs)
+taken = [0] * stars
+nxt = 0
+
+while 0 <= nxt < stars:
+    direction = lambs[nxt] % 2
+    if lambs[nxt] > 0:
+        lambs[nxt] -= 1
+        taken[nxt] = 1
+        tlambs -= 1
+    if direction:
+        nxt += 1
+    else:
+        nxt -= 1
+
+taken = taken.count(1)
+print(taken, tlambs)
