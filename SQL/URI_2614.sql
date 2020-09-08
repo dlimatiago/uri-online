f6977c70d6cb7756f91ff02a4186fb4d1914184c
Tiago Lima <tiaguinho.alima@gmail.com>
2020-09-08 20:06:49 -0300

    Solução dos problemas da URI
    Signed-off-by: Tiago Lima <tiaguinho.alima@gmail.com>

---
+++ SQL/URI_2614.sql
@@ -0,0 +1,5 @@
+select customers.name,
+    rentals.rentals_date
+from customers
+join rentals on rentals.id_customers = customers.id
+where rentals.rentals_date between '2016-09-01' and '2016-09-30';
